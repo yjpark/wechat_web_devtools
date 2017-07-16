345061e632d8186f54fa68df1141738da7249b91
炒饭 <chaofan@2dfire.com>
2017-07-16 11:53:04 +0800

    style:增加注释

--- scripts/replace_package_nw.sh
+++ scripts/replace_package_nw.sh
@@ -8,15 +8,11 @@ sed -ri -e 's/("win32")(\=\=\=process\.platform\|\|)(global\.appConfig\.isDev)/\
 # sensor工具 大小写错误
 sed -ri -e 's/Sensor\/index\.html/sensor\/index\.html/g' "$root_dir/package.nw/app/dist/extensions/devtools.html"
 
-
 # 版本0.18.182200 大小写错误
 sed -ri -e 's/PickerRing\.js/pickerRing\.js/g' "$root_dir/package.nw/app/dist/components/simulator/webview/multiPicker.js"
 
-
+# certificateDetailsPromise
 sed -ri -e 's/t\.target\(\)\.networkManager\.certificateDetailsPromise\(n\.certificateId\)/Promise.resolve(n)/g' "$root_dir/package.nw/app/dist/inject/devtools.js"
-
-
-# TODO certificateDetailsPromise
 
 # 绑定快捷键
 sed -ri -e 's/("win32"===process\.platform),/\1||"linux"===process\.platform,/g' "$root_dir/package.nw/app/dist/common/shortCut/shortCut.js"
