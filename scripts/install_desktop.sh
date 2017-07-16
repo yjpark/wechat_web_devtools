e906b0802a1702fd8e311356e70d0ff012aa01d0
炒饭 <chaofan@2dfire.com>
2017-07-16 11:43:23 +0800

    style

--- scripts/install_desktop.sh
+++ scripts/install_desktop.sh
@@ -3,7 +3,7 @@
 
 root_dir=$(cd `dirname $0`/.. && pwd -P)
 
-sed -r -e 's/\/path\/to/'${root_dir//\//\\\/}'/g' "$root_dir/desktop.example" > "$root_dir/dist/wechat_dev_tools.desktop"
+sed -r -e 's/\/path\/to/'${root_dir//\//\\\/}'/g' "$root_dir/desktop.simple" > "$root_dir/dist/wechat_dev_tools.desktop"
 
 if type desktop-file-install >/dev/null 2>&1; then
   desktop-file-install "$root_dir/dist/wechat_dev_tools.desktop" --dir=$HOME/.local/share/applications
